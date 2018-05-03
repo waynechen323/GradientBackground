944b4241a6690ea749343ac073208dbd23de3af0
Wayne Chen <35021878+waynechen323@users.noreply.github.com>
2018-05-03 14:42:55 +0800

    First commit.

---
+++ app/src/main/java/com/waynechen/gradientbackground/MainActivity.java
@@ -0,0 +1,44 @@
+package com.waynechen.gradientbackground;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ((BottomNavigationView) findViewById(R.id.navigation)).setOnNavigationItemSelectedListener(this);
+        ((BottomNavigationView) findViewById(R.id.navigation)).setSelectedItemId(R.id.navigation_sample1);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.navigation_sample1:
+
+                ((ConstraintLayout) findViewById(R.id.container))
+                        .setBackground(getResources().getDrawable(R.drawable.background_gradient_0, null));
+                return true;
+            case R.id.navigation_sample2:
+
+                ((ConstraintLayout) findViewById(R.id.container))
+                        .setBackground(getResources().getDrawable(R.drawable.background_gradient_45, null));
+                return true;
+            case R.id.navigation_sample3:
+
+                ((ConstraintLayout) findViewById(R.id.container))
+                        .setBackground(getResources().getDrawable(R.drawable.background_gradient_3c, null));
+                return true;
+        }
+        return false;
+    }
+}
